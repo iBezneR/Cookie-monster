0319427c56642e14f5a1203f4037a120df42d840
Your Local Code Dealer <71523355+IgrutinovicL@users.noreply.github.com>
2021-09-27 16:54:43 -0400

    Initial upload of cookie monster.

---
+++ cookieMonster.py
@@ -0,0 +1,20 @@
+import pickle
+import undetected_chromedriver.v2 as uc
+import os
+
+
+
+def Main():
+    os.system("cls")
+    print("Cookie monster ready. Please navigate to a website and log in to save cookies.")
+
+
+    driver = uc.Chrome()
+    name = input("Name for cookie package?\n$")
+    name = "cookie_" + name
+    input("Press enter to save all cookies and quit...")
+    with open(name, 'wb') as filehandler:
+        pickle.dump(driver.get_cookies(), filehandler)
+    driver.quit()
+
+Main()
